84c92add3117e17a94ae4abfaf8d6bbd6ed4ae82
jeru7 <john.ungab7@gmail.com>
2024-05-23 21:42:03 +0800

    add: github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,12 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 function Footer() {
   return (
     <footer className='footer_container'>
-      <div className='footer--user_display'>
-        <FontAwesomeIcon icon={faGithub} className='user-icon' />
-        <p>jeru7</p>
-      </div>
+      <a href='https://github.com/jeru7' target='_blank'>
+        <div className='footer--user_display'>
+          <FontAwesomeIcon icon={faGithub} className='user-icon' />
+          <p>jeru7</p>
+        </div>
+      </a>
     </footer>
   )
 }
